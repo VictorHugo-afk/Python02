8c5b736e7153f2661570e86194754f4a9e8bdc2d
Victor Hugo Ribeiro <ribeiro.santos.victor3007@escola.pr.gov.br>
2026-02-12 11:03:42 -0300

    Create teste.py
    12/02/2026

---
+++ teste.py
@@ -0,0 +1,12 @@
+# Variavel e um nome que você da para algum valor ou alguma coisa específica, evitar todas as vezes que precisar digitar algo (este é um comentário)
+faturamento = 1000
+custo = 700
+lucro = faturamento - custo
+print ('O faturamento foi de', 1000)
+print ('O custo foi de', 700)
+print ('O lucro foi de', 1000 - 700)
+# As aspas só servem para escrever textos e inserir vírgula (este é um comentario)
+# int números inteiros 
+# variavel booleano descobre se é verdadeiro ou falso, true ou false
+# número com casa decimal flat ou números quebrados
+teve lucro_
